cbbd7a48be5a3bece5f66fa176e775d830cdd66b
Jon Martin Stuberg <jm.stuberg00@gmail.com>
2021-12-31 17:35:39 +0100

    Created middleware, fixed regchecking

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const FinnLookup = require('./lib/lookup');
-const requiredParameters = require('./lib/middleware');
-const StatensVegvesenVehicle = require('./lib/vegvesen');
+const FinnLookup = require("./lib/lookup");
+const { requiredParameters, verifyRegNr } = require("./lib/middleware");
+const StatensVegvesenVehicle = require("./lib/vegvesen");
 
-app.get('/finn/lookup', requiredParameters(['regNr', 'milage']), async (req, res) => {
-	console.log(Object.entries(req.query));
-	const lookup = new FinnLookup({
-		regNr: req.query.regNr,
-		milage: req.query.milage,
-	});
-	const results = await lookup.getResults();
+app.get(
+	"/finn/lookup",
+	requiredParameters(["regNr", "milage"]),
+	verifyRegNr("regNr", true),
+	async (req, res) => {
+		console.log(Object.entries(req.query));
+		const lookup = new FinnLookup({
+			regNr: req.query.regNr,
+			milage: req.query.milage,
+		});
+		const results = await lookup.getResults();
 
-	res.json(results);
-	// }
-});
+		res.json(results);
+	}
+);
 
-app.get('/vegvesen/reg', async (req, res) => {
-	const vehicle = new StatensVegvesenVehicle({
-		vin: req.query.vin,
-	});
+app.get(
+	"/vegvesen/reg",
+	requiredParameters(["vehicle"]),
+	verifyRegNr("vehicle"),
+	async (req, res) => {
+		const vehicle = new StatensVegvesenVehicle({
+			vin: req.vin,
+			regNr: req.regNr,
+		});
 
-	const results = await vehicle.getData();
-	res.json(results);
-});
+		const results = await vehicle.getData();
+		res.json(results);
+	}
+);
 
 app.listen(process.env.port, () => {
 	console.log(`🚀 App started on http://localhost:${process.env.port}`);
